5012ca4933c73c98122e2790b3cb2792898b9067
Filip Nikolic <filip.nikolic@htecgroup.com>
2023-07-29 00:25:33 +0200

    Add eslint + prettier

--- App.tsx
+++ App.tsx
@@ -1,37 +1,40 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
-import {MD3LightTheme as DefaultTheme, PaperProvider} from "react-native-paper";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+} from "react-native-paper";
 
 const theme = {
-    ...DefaultTheme,
-    colors: {
-        ...DefaultTheme.colors,
-        primary: 'tomato',
-        secondary: 'yellow',
-    },
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "tomato",
+    secondary: "yellow",
+  },
 };
 export default function App() {
-    return (
-        <PaperProvider theme={theme}>
-            <AppContent />
-        </PaperProvider>
-    );
+  return (
+    <PaperProvider theme={theme}>
+      <AppContent />
+    </PaperProvider>
+  );
 }
 
-const AppContent = ()=> {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto"/>
-        </View>
-    )
-}
+const AppContent = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
