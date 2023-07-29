de8f6ca5c6ade3ec3aa0d8eae1abd6dcd4f7f29d
Filip Nikolic <filip.nikolic@htecgroup.com>
2023-07-29 19:52:04 +0200

    Create base text component and extend it on Body

--- src/components/text/Body.tsx
+++ src/components/text/Body.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components/native";
 
-export const Body = styled.Text<{ bold?: boolean }>`
+import { Text } from "./Text";
+
+export const Body = styled(Text)<{ bold?: boolean }>`
   font-weight: ${({ bold = false }) => (bold ? 600 : 400)};
   font-size: 16px;
   line-height: 24px;
