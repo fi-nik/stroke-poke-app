799302c34a378f6dbc447cf7f7cbcb429a5b8593
Filip Nikolic <filip.nikolic@htecgroup.com>
2023-07-29 19:52:13 +0200

    Create Headline text component

---
+++ src/components/text/Headline.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+import { Text } from "./Text";
+
+export const Headline = styled(Text)`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 24px;
+`;
